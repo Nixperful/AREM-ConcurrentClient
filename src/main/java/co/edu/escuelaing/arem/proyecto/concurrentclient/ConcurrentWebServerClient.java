dea030193bc0a5f8642a57efb8d1ab03b345286a
nixperful <nicolasosorioarias@hotmail.com>
2018-09-21 14:31:59 -0500

    Primera Version Cliente

---
+++ src/main/java/co/edu/escuelaing/arem/proyecto/concurrentclient/ConcurrentWebServerClient.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.escuelaing.arem.proyecto.concurrentclient;
+
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Nicolás Osorio 
+ */
+public class ConcurrentWebServerClient {
+    public static Integer threads= 5;
+    
+    public static void main(String[] args) throws IOException {
+        
+         
+        
+        
+        
+        boolean isCompleted=false;
+        
+        ExecutorService executor = Executors.newFixedThreadPool(threads);
+        while (!isCompleted){
+            executor.execute(new URLReader()));
+        }
+        
+        try {
+            serverSocket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ConcurrentWebServerClient.class.getName()).log(Level.SEVERE, null, ex);
+        }   
+    }
+    
+    
+    
+}
---
+++ src/main/java/co/edu/escuelaing/arem/proyecto/concurrentclient/URLReader.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.escuelaing.arem.proyecto.concurrentclient;
+import java.io.*; 
+import java.net.*; 
+/**
+ *
+ * @author 2110432
+ */
+
+public class URLReader { 
+  public void readURL(String[] args) throws Exception { 
+      URL url = new URL(args[0]); 
+      try (BufferedReader reader = new BufferedReader(
+          new InputStreamReader(url.openStream()))) { 
+            String inputLine = null; 
+            while ((inputLine = reader.readLine()) != null) { 
+                  System.out.println(inputLine); 
+             } 
+       } catch (IOException x) { 
+               System.err.println(x); 
+       } 
+    } 
+}
